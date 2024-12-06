9c363f748cdb0f27aca3042c05560eb6d1e4ec7d
Senibo Don-Pedro <senibodonpedro@gmail.com>
2024-12-06 07:29:26 +0000

    built a working client and server side form

--- actions/submit-form.ts
+++ actions/submit-form.ts
@@ -5,16 +5,29 @@ import { formSchema } from "./form-schemas";
 
 export type FormState = {
     message: string;
+    fields?: Record<string, string>
 }
 
-export async function submitFormAction(data: FormData): Promise<FormState> {
+export async function submitFormAction(prevState: FormState,data: FormData): Promise<FormState> {
     const formData = Object.fromEntries(data)
 
     const parsed = formSchema.safeParse(formData)
 
     if (!parsed.success) {
+        const fields:Record<string,string> = {}
+        for(const key of Object.keys(formData)) {
+            fields[key] = formData[key].toString()
+        }
         return {
             message: "Invalid form data",
+            fields
+        }
+    }
+
+    if (parsed.data.username.includes("a")) {
+        return {
+            message: "Invalid username, it contains an A",
+            fields: parsed.data,
         }
     }
 
--- components/form-component.tsx
+++ components/form-component.tsx
@@ -17,75 +17,82 @@ import { Input } from "@/components/ui/input"
 import { Loader2 } from "lucide-react"
 import { submitFormAction } from "@/actions/submit-form"
 import { formSchema } from "@/actions/form-schemas" 
+import { useActionState, useRef } from "react"
+
+
 
 
 export function ProfileForm() {
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        username: "",
-        password: ""
-      },
-    })
-   
-    // 2. Define a submit handler.
-    async function onSubmit(values: z.infer<typeof formSchema>) {
-      //fake loading
-      // await new Promise((resolve) => setTimeout(resolve, 2000))
-      // Do something with the form values.
-      // ✅ This will be type-safe and validated.
 
-      //append to formData
-      const formData = new FormData()
-      formData.append("username",values.username)
-      formData.append("password",values.password)
+  const [state, Action] = useActionState(submitFormAction, {
+    message: "",
+  })
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+      ...(state.fields ?? {})
+    },
+  })
+  
 
-      console.log(await submitFormAction(formData))
-    }
+  const {isSubmitting} = form.formState
 
-    const {isSubmitting} = form.formState
+  const formRef = useRef<HTMLFormElement>(null);
 
-    return (
-        <div className="flex justify-center items-center">
-          <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="w-full max-w-sm bg-white p-8 rounded-lg shadow-lg space-y-6">
-                  <FormField
-                      control={form.control}
-                      name="username"
-                      render={({ field }) => (
-                          <FormItem>
-                              <FormLabel>Username</FormLabel>
-                              <FormControl>
-                                  <Input placeholder="This is your public display name." {...field} />
-                              </FormControl>
-                              <FormMessage />
-                          </FormItem>
-                      )}
-                  />
-                  <FormField
-                      control={form.control}
-                      name="password"
-                      render={({ field }) => (
-                          <FormItem>
-                              <FormLabel>Password</FormLabel>
-                              <FormControl>
-                                  <Input placeholder="Input your password" {...field} />
-                              </FormControl>
-                              <FormMessage />
-                          </FormItem>
-                      )}
-                  />
-                  <Button type="submit" disabled={isSubmitting} className="w-full">
-                    {isSubmitting ?
-                      <div className="flex justify-center items-center gap-2">
-                        <Loader2 className="animate-spin" />
-                        Please wait
-                      </div>
-                    : "Submit"}
-                  </Button>
-              </form>
-          </Form>
-        </div>
-    )
+  return (
+      <div className="flex justify-center items-center">
+        <Form {...form}>
+            <form
+              ref={formRef} 
+              action={Action} 
+              onSubmit={form.handleSubmit(() => formRef.current?.submit())} 
+              className="w-full max-w-sm bg-white p-8 rounded-lg shadow-lg space-y-6"
+            >
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="This is your public display name." {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Input your password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" disabled={isSubmitting} className="w-full">
+                  {isSubmitting ?
+                    <div className="flex justify-center items-center gap-2">
+                      <Loader2 className="animate-spin" />
+                      Please wait
+                    </div>
+                  : "Submit"}
+                </Button>
+
+                {
+                  state?.message !== "" && (
+                    <div className="text-red-500">{state.message}</div>
+                  )
+                }
+            </form>
+        </Form>
+      </div>
+  )
 }
